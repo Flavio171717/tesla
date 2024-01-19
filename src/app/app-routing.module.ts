57857435c9a9e3d0ead94a3bf505ab6653e54441
Flavio Bonfitto <flavio.bonfitto@zorgi.be>
2024-01-19 16:45:57 +0100

    fbo-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,10 @@ import { OrderStepTwoComponent } from './features/order/components/order/order-s
 
 export const routes: Routes = [
   {path: '', loadChildren: () => import('./features/order/order.module').then(m => m.OrderModule)},
-  {path: 'order', loadChildren: () => import('./features/order/order.module').then(m => m.OrderModule)},
-  {path: 'order-step-one', component: OrderStepOneComponent},
-  {path: 'order-step-two', component: OrderStepTwoComponent},
-  {path: 'order-step-three', component: OrderStepThreeComponent},
+  // {path: 'order', loadChildren: () => import('./features/order/order.module').then(m => m.OrderModule)},
+  // {path: 'order-step-one', component: OrderStepOneComponent},
+  // {path: 'order-step-two', component: OrderStepTwoComponent},
+  // {path: 'order-step-three', component: OrderStepThreeComponent},
   {path: '**', redirectTo: ''}
 ];
 
-
-@NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(),
     FormsModule
   ]
